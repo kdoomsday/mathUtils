02b4c5bee8ef639b7cb27ac7d9b8ec511100077b
Eduardo Barrientos <kdoomsday@gmail.com>
2026-05-04 11:16:05 -0400

    Load matrix from resource

--- src/main/scala/file/LoadObject.scala
+++ src/main/scala/file/LoadObject.scala
@@ -1,12 +1,25 @@
 package file
 
 object LoadObject {
+  /**
+   * Reads a line and returns the numbers in it
+   *
+   * @param line The line
+   * @param sep Separator expected between the numbers
+   * @return Array of numbers in the line
+   */
+  private def numerizer(line: String, sep: String): Array[Int] = line.split(sep).filterNot(_ == "").map(_.toInt)
+
   /** Load an Int matrix from a file */
   def loadMatrix(filename: String, sep: String = ","): Array[Array[Int]] = {
-    def numerizer(line: String): Array[Int] = line.split(sep).filterNot(_ == "").map(_.toInt)
-
     val lines = scala.io.Source.fromFile(filename).getLines
+    lines.toArray.map(line => numerizer(line, sep))
+  }
 
-    lines.toArray.map(numerizer)
+  /** Load an Int matrix from a file */
+  def loadMatrixResource(resource: String, sep: String = ","): Array[Array[Int]] = {
+    val cl = Thread.currentThread().getContextClassLoader()
+    val lines = scala.io.Source.fromInputStream(cl.getResourceAsStream(resource)).getLines
+    lines.toArray.map(line => numerizer(line, sep))
   }
 }
